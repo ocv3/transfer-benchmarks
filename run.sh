15e2adfca12dd65d19c636592ac5a7e98335f4d7
ocv3 <ov3@sanger.ac.uk>
2026-02-05 17:10:45 +0000

    Add dry-run arg

--- run.sh
+++ run.sh
@@ -2,20 +2,23 @@
 
 remote_test_dirs=('rcs-ajt208-server-mirror/cashew/home/sanbot/HAP1HCT' 'rcs-ajt208-server-mirror/coconut/var/www/tom/Logos/' 'rcs-ajt208-server-mirror/nutcase/wrk/data/bsahu/LoVo_Hep-TF_ChIP-seq/macs2/')
 s3_path="ov3-transfer-test/test-transfer"
+dry_run=$([ "$3" == "--dry-run" ] && echo "--dry-run")
+
 
 function test_transfer() {
   c=0
   avg=0
   for remote_dir in "${remote_test_dirs[@]}"; do
+    ((c+=1))
+    echo "RUN $((c))/${#remote_test_dirs[@]}: $remote_dir -> $1"
     sleep 1
     startTime=$(date +%s)
 
-    sshpass -p "$2" rsync -a -h --dry-run is525@rds.uis.cam.ac.uk:"$remote_dir" "$1"
+    sshpass -p "$2" rsync -av -h "$dry_run" is525@rds.uis.cam.ac.uk:"$remote_dir" "$1"
 
     delta=$(("$(date +%s) - $startTime"))
     ((avg+=delta))
-    ((c+=1))
-    echo "RUN $((c))/${#remote_test_dirs[@]}($delta): $remote_dir -> $1"
+    echo "RUN $((c))/${#remote_test_dirs[@]} TIME TOOK: $delta seconds: $remote_dir -> $1"
   done
   echo "DOWNLOAD AVG FOR Tape station -> $1: $(( avg / ${#remote_test_dirs[@]})) seconds"
 }
@@ -47,26 +50,36 @@ function wrMount() {
   echo $serverPID
 }
 
+function prep_env() {
+  if [ "$1" == "headnode" ]; then
+    module load rclone-1.65.1/perl-5.38.0
+  fi
+}
 
+function clear_s3_remote() {
+  rclone delete -v "$dry_run" "ov3-s3:$s3_path"
+}
 
 function test_s3_tool() {
   # 1 Tool
   # 2 Source Local
   # 3 Optional transfer
+  echo "S3 TRANSFER TOOL TEST($1): $2 -> s3://$s3_path/"
   startTime=$(date +%s)
   if [ "$1" == "s5cmd" ]; then
-    s5cmd --dry-run --endpoint-url https://cog.sanger.ac.uk cp "$2" "s3://$s3_path/"
+    s5cmd "$dry_run" --endpoint-url https://cog.sanger.ac.uk cp "$2" "s3://$s3_path/"
   elif [ "$1" == "rclone" ]; then
-    rclone copy --dry-run "$2" ov3-s3:$s3_path
+    rclone copy "$dry_run" "$2" -v "ov3-s3:$s3_path"
   elif [ "$1" == "aws" ]; then
-    aws s3 --endpoint-url=https://cog.sanger.ac.uk cp --dryrun --recursive "$2" "s3://$s3_path"
+    dr2=$([ "$dry_run" == "--dry-run" ] && dr2="--dryrun")
+    aws s3 --endpoint-url=https://cog.sanger.ac.uk cp "$dr2" -v --recursive "$2" "s3://$s3_path"
   elif [ "$1" == "wrMount" ]; then
     wrMountPID=$(wrMount "$2" "$s3_path" "true" "false")
     $3
     kill "$wrMountPID"
   fi
   delta=$(("$(date +%s) - $startTime"))
-  echo "UPLOAD TIME:$delta"
+  echo "S3 TRANSFER TOOL TEST($1) TIME TOOK: $delta seconds : $2 -> s3://$s3_path/"
 }
 
 
@@ -78,16 +91,16 @@ if [ "$1" == "openstack" ]; then
   #   Openstack ssd -> direct S3
   local_dest_dirs=("/tmp/test-transfer" "/home/ubuntu/volume-mount/test-transfer")
   s3_tools=('rclone' 'aws' 's5cmd')
+  prep_env "$1"
 
   for local_dest in "${local_dest_dirs[@]}"; do
-    echo "TEST FOR: Tape station -> $local_dest"
+    echo "TEST FOR $1: Tape station -> $local_dest"
     mkdir -p "$local_dest"
     test_transfer "$local_dest" "$2"
 
     for tool in "${s3_tools[@]}"; do
-      echo "TESTING S3 TOOL $tool"
       test_s3_tool "$tool" "$local_dest/"
-      rclone purge --dry-run "ov3-s3:$s3_path"
+      clear_s3_remote
     done
 
     clean_dir "$local_dest"
@@ -96,7 +109,16 @@ elif [ "$1" == "headnode" ]; then
   # Will test
   #   Tape station -> head node lustre
   local_dest="/lustre/scratch126/gengen/teams/hgi/ov3/taipale_tapestation/test-transfer"
-  echo "TEST FOR: Tape station -> $local_dest"
+  s3_tools=('rclone' 'aws' 's5cmd')
+  prep_env "$1"
+
+  echo "TEST FOR $1: Tape station -> $local_dest"
   test_transfer "$local_dest" "$2"
+
+  for tool in "${s3_tools[@]}"; do
+    test_s3_tool "$tool" "$local_dest/"
+    clear_s3_remote
+  done
+
   clean_dir "$local_dest"
 fi
