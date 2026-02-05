83f5ff8ed799ad881d12f104f9e7e1e9884aaed7
ocv3 <ov3@sanger.ac.uk>
2026-02-05 11:37:17 +0000

    Extract into functions

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-dir=$(dirname "$0")
-transfer_dest_dir=~/transfer
 directories=('rcs-ajt208-server-mirror/cashew/home/sanbot/HAP1HCT' 'rcs-ajt208-server-mirror/coconut/var/www/tom/Logos/' 'rcs-ajt208-server-mirror/nutcase/wrk/data/bsahu/LoVo_Hep-TF_ChIP-seq/macs2/')
 
 function test_transfer() {
   c=0
   avg=0
   for remote_dir in "${directories[@]}"; do
-    mkdir -p "$1"
     sleep 1
     startTime=$(date +%s)
 
@@ -18,11 +15,15 @@ function test_transfer() {
     ((avg+=delta))
     ((c+=1))
     echo "RUN $((c))/${#directories[@]}($delta): $remote_dir -> $1"
-    echo "Cleaning transfer dir: $1..."
-    rm -rf "$1"
   done
-  echo $avg
+  echo "DOWNLOAD AVG FOR Tape station -> $1: $(( avg / ${#directories[@]})) seconds"
 }
+
+function clean_dir() {
+  echo "CLEANING: Removing $1..."
+  rm -r "$1"
+}
+
 
 
 if [[ $1 == "openstack" ]]; then
@@ -32,7 +33,9 @@ if [[ $1 == "openstack" ]]; then
   dest_dirs=("/tmp/test-transfer" "/home/ubuntu/volume-mount/test-transfer")
   for dest in "${dest_dirs[@]}"; do
     echo "TEST FOR: Tape station -> $dest"
+    mkdir -p "$dest"
     test_transfer "$dest" "$2"
+    clean_dir "$dest"
   done
 elif [[ $1 == "headnode" ]]; then
   # Will test
@@ -40,10 +43,5 @@ elif [[ $1 == "headnode" ]]; then
   dest="/lustre/scratch126/gengen/teams/hgi/ov3/taipale_tapestation/test-transfer"
   echo "TEST FOR: Tape station -> $dest"
   test_transfer "$dest" "$2"
+  clean_dir "$dest"
 fi
-
-
-
-
-echo "$dir" $((avg/3)) "$transfer_dest_dir"
-
