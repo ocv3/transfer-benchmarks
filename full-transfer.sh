215ba7e104d9c46486f72d836d79bdb49faf90ae
ocv3 <ov3@sanger.ac.uk>
2026-02-11 10:26:54 +0000

    Add more logging

--- full-transfer.sh
+++ full-transfer.sh
@@ -9,6 +9,12 @@ function echo-log() {
   printf "%s SCRIPT-OUT: %s\n" "$time" "$1" | gzip -9 > script-out.gz
 }
 
+function echo-status() {
+  time=$(date '+%Y-%m-%d %H:%M:%S')
+  printf "%s SCRIPT-OUT: \n\tAMOUNT TRANSFERRED: %s\n%s\n" "$time" "$1" "$2"
+  printf "%s SCRIPT-OUT: \n\tAMOUNT TRANSFERRED: %s\n%s\n" "$time" "$1" "$2" | gzip -9 > script-out.gz
+}
+
 echo-log "BEGINNING TRANSFER: $remote_dir -> $dest_dir"
 sshpass -p "$1" rsync -P -av -h is525@rds.uis.cam.ac.uk:$remote_dir $dest_dir &
 transferPID=$!
@@ -16,7 +22,7 @@ transferPID=$!
 while [ "$(ps -p $transferPID | wc -l)" == "2" ]; do
   size=$(du -sh . | cut -f1)
   inodes=$(df -i ~/volume-mount/ | awk '{print $3}')
-  echo-log "\n$time\nAMOUNT TRANSFERRED: $size\n$inodes"
+  echo-status "$size" "$inodes"
   sleep 15
 done
 
