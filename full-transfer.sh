d6b52751c018e12ea6601aec8825e24af920dde4
ocv3 <ov3@sanger.ac.uk>
2026-02-11 11:13:55 +0000

    Fix output

--- full-transfer.sh
+++ full-transfer.sh
@@ -6,27 +6,29 @@ dest_dir=/home/ubuntu/volume-mount/full-transfer
 function echo-log() {
   time=$(date '+%Y-%m-%d %H:%M:%S')
   printf "%s SCRIPT-OUT: %s\n" "$time" "$1"
-  printf "%s SCRIPT-OUT: %s\n" "$time" "$1" | gzip -9 > script-out.gz
+  printf "%s SCRIPT-OUT: %s\n" "$time" "$1" | gzip -9 >> script-out.gz
 }
 
 function echo-status() {
   time=$(date '+%Y-%m-%d %H:%M:%S')
   printf "%s SCRIPT-OUT: \nAMOUNT TRANSFERRED: %s\n%s\n" "$time" "$1" "$2"
-  printf "%s SCRIPT-OUT: \nAMOUNT TRANSFERRED: %s\n%s\n" "$time" "$1" "$2" | gzip -9 > script-out.gz
+  printf "%s SCRIPT-OUT: \nAMOUNT TRANSFERRED: %s\n%s\n" "$time" "$1" "$2" | gzip -9 >> script-out.gz
 }
 
 echo-log "BEGINNING TRANSFER: $remote_dir -> $dest_dir"
-sshpass -p "$1" rsync -P -av -h is525@rds.uis.cam.ac.uk:$remote_dir $dest_dir &
+sshpass -p "$1" rsync -P -av -h is525@rds.uis.cam.ac.uk:$remote_dir $dest_dir 1> >(gzip -9 >> res.gz) 2> >(gzip -9 >> errors.gz) &
 transferPID=$!
 
-while [ "$(ps -p $transferPID | wc -l)" == "2" ]; do
-  size=$(du -sh . | cut -f1)
-  inodes=$(df -i ~/volume-mount/ | awk '{print $3}')
-  echo-status "$size" "$inodes"
-  sleep 15
+
+while sleep 15; do
+  case "$(ps -p $transferPID | wc -l)" in
+  2)
+    size=$(du -sh . | cut -f1)
+    inodes=$(df -i ~/volume-mount/ | awk '{print $3}')
+    echo-status "$size" "$inodes"
+    ;;
+  1)
+    echo-log "TRANSFER DIED"
+    ;;
+  esac
 done
-
-echo-log "TRANSFER DIED"
-
-
-
