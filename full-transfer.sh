b8dc478c332e0e2b124f7483f03137b45bf7cef5
ocv3 <ov3@sanger.ac.uk>
2026-02-11 09:43:02 +0000

    Add logging to transfer

--- full-transfer.sh
+++ full-transfer.sh
@@ -3,4 +3,24 @@
 remote_dir=rcs-ajt208-server-mirror/
 dest_dir=/home/ubuntu/volume-mount/full-transfer
 
-sshpass -p "$1" rsync -P -av -h is525@rds.uis.cam.ac.uk:$remote_dir $dest_dir
+function echo-log() {
+  time=$(date '+%Y-%m-%d %H:%M:%S')
+  printf "%s SCRIPT-OUT: %s\n" "$time" "$1"
+  printf "%s SCRIPT-OUT: %s\n" "$time" "$1" | gzip -9 > script-out.gz
+}
+
+echo-log "BEGINNING TRANSFER: $remote_dir -> $dest_dir"
+sshpass -p "$1" rsync -P -av -h is525@rds.uis.cam.ac.uk:$remote_dir $dest_dir &
+transferPID=$!
+
+while [ "$(ps -p $transferPID | wc -l)" == "2" ]; do
+  size=$(du -sh . | cut -f1)
+  inodes=$(df -i ~/volume-mount/ | awk '{print $3}')
+  echo-log "\n$time\nAMOUNT TRANSFERRED: $size\n$inodes"
+  sleep 15
+done
+
+echo-log "TRANSFER DIED"
+
+
+
